442c75035aee7401481a78246a9a77eece5e3917
Brian <brianmkcebm@gmail.com>
2021-01-04 20:25:39 +0000

    tweak codelist address

--- analysis/codelists.py
+++ analysis/codelists.py
@@ -1,5 +1,5 @@
 nhse_care_home_des_codes = codelist_from_csv(
-    "codelists/opensafely-nhs-england-care-homes-residential-status-ctv3-1e037bde.csv",
+    "codelists/opensafely-nhs-england-care-homes-residential-status-ctv3.csv",
     system="ctv3",
     column="code",
 )
