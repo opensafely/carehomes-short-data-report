befa04bf6f9c94322f77dca3ed80c72f34165739
Brian <brianmkcebm@gmail.com>
2021-01-04 20:18:19 +0000

    add ethnic codelist

--- analysis/codelists.py
+++ analysis/codelists.py
@@ -1,3 +1,19 @@
 nhse_care_home_des_codes = codelist_from_csv(
-    "codelists/opensafely-nhs-england-care-homes-residential-status-ctv3-1e037bde.csv", system="ctv3", column="code",
+    "codelists/opensafely-nhs-england-care-homes-residential-status-ctv3-1e037bde.csv",
+    system="ctv3",
+    column="code",
+)
+
+
+ethnicity_codes = codelist_from_csv(
+    "codelists/opensafely-ethnicity.csv",
+    system="ctv3",
+    column="Code",
+    category_column="Grouping_6",
+)
+ethnicity_codes_16 = codelist_from_csv(
+    "codelists/opensafely-ethnicity.csv",
+    system="ctv3",
+    column="Code",
+    category_column="Grouping_16",
 )
